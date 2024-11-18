1db32467eadc7e1e35f941216232646296491c91
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-18 16:17:03 +0700

    feat: adding background image to user layout

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,15 +1,22 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
 
 interface UserLayoutProps {
   children: ReactNode;
+  background?: boolean;
 }
 
-const UserLayout: React.FC<UserLayoutProps> = ({ children }) => {
+const UserLayout: React.FC<UserLayoutProps> = ({ children, background = true }) => {
   return (
-    <div className='flex justify-center h-screen py-24 px-12'>
+    <div
+      className={
+        background
+          ? "flex justify-center h-screen py-24 px-12 bg-user-bg bg-cover bg-right-bottom"
+          : "flex justify-center h-screen py-24 px-12"
+      }
+    >
       <main>{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default UserLayout
+export default UserLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'user-bg': "url('/src/assets/images/User Background.png')",
+      },
+    },
   },
   plugins: [],
 }
