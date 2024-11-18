e4ed7022c103c03faee7ef13036376c8683ba8be
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-18 17:02:47 +0700

    feat: initialize atomic structure for component files and folders

---
+++ src/components/atoms/ButtonCircle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonCircle: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ButtonCircle
---
+++ src/components/atoms/ButtonSecondary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonSecondary: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ButtonSecondary
