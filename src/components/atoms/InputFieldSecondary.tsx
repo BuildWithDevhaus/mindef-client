1456df1a25721616982e6fc85028f04dd45dc8ea
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-19 23:53:50 +0700

    refactor: cleaning up

--- src/components/atoms/InputFieldSecondary.tsx
+++ src/components/atoms/InputFieldSecondary.tsx
@@ -1,20 +1,24 @@
 interface InputFieldSecondaryProps {
-  placeholder: string
-  className?: string
-  type?: 'disabled' | 'enabled'
+  placeholder: string;
+  className?: string;
+  type?: "disabled" | "enabled";
 }
 
-const InputFieldSecondary: React.FC<InputFieldSecondaryProps> = ({ placeholder, className, type = 'enabled' }) => {
-  const isDisabled = type === 'disabled'
+const InputFieldSecondary: React.FC<InputFieldSecondaryProps> = ({
+  placeholder,
+  className,
+  type = "enabled",
+}) => {
+  const isDisabled = type === "disabled";
 
   return (
     <input
       type="text"
       className={`text-center text-base w-full py-[10px] px-[14px] border bg-[#FFFFFF] border-[#CBD5E1] rounded-lg focus:outline-none focus:border-[#2F6D57] ${className}`}
       placeholder={placeholder}
-      disabled={isDisabled} 
+      disabled={isDisabled}
     />
-  )
-}
+  );
+};
 
-export default InputFieldSecondary
+export default InputFieldSecondary;
