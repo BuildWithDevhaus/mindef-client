ffe2f4918cd5ec8c363320ecc47fa812fd69c5e9
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-19 22:45:47 +0700

    refactor: cleaning up input field

--- src/components/atoms/InputFieldPrimary.tsx
+++ src/components/atoms/InputFieldPrimary.tsx
@@ -1,19 +1,21 @@
-import React, { ReactNode } from 'react'
+import React from "react";
 
 interface InputFieldPrimaryProps {
-  placeholder: string
-  className?: string
+  placeholder: string;
+  className?: string;
 }
 
-const InputFieldPrimary: React.FC<InputFieldPrimaryProps> = ( { placeholder, className} ) => {
+const InputFieldPrimary: React.FC<InputFieldPrimaryProps> = ({
+  placeholder,
+  className,
+}) => {
   return (
     <input
-    type="text"
-    className={`text-center text-align-center text-[36px] w-full py-4 px-4 border bg-[#F5F5F5] border-[#B7B7B7] rounded-lg shadow-md focus:outline-none focus:border-[#2F6D57] ${className}`}
-    placeholder={placeholder}
-  />
+      type="text"
+      className={`text-center text-align-center text-[36px] w-full py-4 px-4 border bg-[#F5F5F5] border-[#B7B7B7] rounded-lg shadow-md focus:outline-none focus:border-[#2F6D57] ${className}`}
+      placeholder={placeholder}
+    />
+  );
+};
 
-  )
-}
-
-export default InputFieldPrimary
+export default InputFieldPrimary;
