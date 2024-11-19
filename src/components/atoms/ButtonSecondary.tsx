2f6576c3efc425bbf34977b1f05043fa9d184c6f
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-19 22:46:53 +0700

    refactor: cleaning up button secondary

--- src/components/atoms/ButtonSecondary.tsx
+++ src/components/atoms/ButtonSecondary.tsx
@@ -4,12 +4,15 @@ interface ButtonSecondaryProps {
   children: ReactNode;
   onClick?: () => void;
 }
+
 const ButtonSecondary: React.FC<ButtonSecondaryProps> = ({ children, onClick }) => {
   return (
-    <button className="bg-[#ffffff] text-[#344054] hover:bg-[#ebebeb] font-semibold py-2 px-4 border-solid border-2 border-[#2F6D57] rounded-xl"
-    onClick={onClick}>
-    {children}
-  </button>
+    <button
+      className="bg-[#ffffff] text-[#344054] hover:bg-[#ebebeb] font-semibold py-2 px-4 border-solid border-2 border-[#2F6D57] rounded-xl"
+      onClick={onClick}
+    >
+      {children}
+    </button>
   )
 }
 
