3aa785d9258f2a7d9f3a39b72d4cbe756c05501d
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-11-20 18:51:31 +0800

    feat: adding button icon and the close/edit icons

--- src/types/button.d.ts
+++ src/types/button.d.ts
@@ -2,3 +2,9 @@ interface ButtonProps {
   children: ReactNode;
   onClick?: () => void;
 }
+
+interface ButtonIconProps {
+  icon: 'close' | 'pencil';
+  onClick?: () => void;
+  className?: string;
+}
