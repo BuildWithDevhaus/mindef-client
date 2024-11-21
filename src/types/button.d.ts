4f7d1068400248c15fd0374bb7cdca3c73a3d177
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-21 11:58:28 +0700

    refactor: updating button types

--- src/types/button.d.ts
+++ src/types/button.d.ts
@@ -1,11 +1,10 @@
 interface ButtonProps {
   children: ReactNode;
   onClick?: () => void;
+  className?: string;
   disabled?: boolean;
 }
 
-interface ButtonIconProps {
+interface ButtonIconProps extends ButtonProps {
   icon: 'close' | 'pencil';
-  onClick?: () => void;
-  className?: string;
 }
