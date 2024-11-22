c000e9245ccdb67cc26182547a54e3f59d67a797
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-11-22 10:32:24 +0800

    feat: add logout link hover effect

--- src/components/molecules/LogoutButton.tsx
+++ src/components/molecules/LogoutButton.tsx
@@ -12,7 +12,7 @@ const LogoutButton: React.FC<ButtonProps> = ({ children, onClick, className }) =
     <div className="w-full border-t border-gray-300 pt-4">
       <button
         onClick={onClick}
-        className={`w-full flex items-center justify-between px-2 text-sm font-medium text-gray-600 hover:text-green-700 transition-colors ${className}`}
+        className={`w-full flex items-center justify-between px-2 text-sm font-medium text-gray-600 hover:text-green-700 transition-colors hover:font-semibold ${className}`}
       >
         <span className="ml-2">{children}</span>
         <IconLogout />
