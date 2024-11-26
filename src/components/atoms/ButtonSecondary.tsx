7f49d07d12ecef15dd3fbffa84ea169ded631b4b
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-26 11:40:30 +0700

    feat: adding variant to button secondary

--- src/components/atoms/ButtonSecondary.tsx
+++ src/components/atoms/ButtonSecondary.tsx
@@ -6,10 +6,13 @@ const ButtonSecondary: React.FC<ButtonProps> = ({
   onClick,
   disabled,
   className,
+  variant = "default",
 }) => {
+  const renderSize = variant === "default" ? "text-[13px] py-[10px] px-5 rounded-md" : "text-[26px] px-10 py-5 rounded-xl";
+
   return (
     <button
-      className={clsx(`bg-[#ffffff] text-[#344054] hover:bg-[#ebebeb] font-semibold py-2 px-4 border-solid border-2 border-[#2F6D57] rounded-xl ${
+      className={clsx(`bg-[#ffffff] text-[#344054] hover:bg-[#ebebeb] font-semibold py-2 px-4 border-solid border-2 border-[#2F6D57] ${renderSize} ${
         disabled
           ? "disabled:opacity-50 hover:bg-[#ffffff] cursor-not-allowed"
           : ""
