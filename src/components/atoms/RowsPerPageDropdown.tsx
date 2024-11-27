63bdc913b06b2a41efc60eba0251914ea70b3b9c
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-11-27 10:50:21 +0800

    style: adjusting styles according to figma

--- src/components/atoms/RowsPerPageDropdown.tsx
+++ src/components/atoms/RowsPerPageDropdown.tsx
@@ -3,21 +3,21 @@ import React from 'react'
 const RowsPerPageDropdown: React.FC<RowsPerPageDropdownProps> = ( {rowsPerPage,  onRowsPerPageChange}) => {
   return (
     <div className="flex items-center space-x-2">
-      <label htmlFor="rowsPerPage" className="text-sm">
+      <label htmlFor="rowsPerPage" className="text-sm font-semibold text-[#344054]">
         Show
       </label>
       <select
         id="rowsPerPage"
         value={rowsPerPage}
         onChange={(e) => onRowsPerPageChange(Number(e.target.value))}
-        className="px-2 py-1 border rounded"
+        className="px-2 py-1 border rounded font-semibold text-[#1D243C] min-h-12"
       >
         <option value={5}>5</option>
         <option value={10}>10</option>
         <option value={25}>25</option>
         <option value={50}>50</option>
       </select>
-      <span className="text-sm">Entries</span>
+      <span className="text-sm font-semibold text-[#344054]">Entries</span>
     </div>
   )
 }
