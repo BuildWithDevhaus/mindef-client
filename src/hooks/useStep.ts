d4d975dd483ffaf7d04ffec324393dea1b3cb977
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-27 14:57:39 +0700

    fix: unusual behavior when clicking back button multiple times

--- src/hooks/useStep.ts
+++ src/hooks/useStep.ts
@@ -1,36 +1,44 @@
 import { atom, useAtom } from "jotai";
 
 const stepAtom = atom("scan-nric");
-const prevStepAtom = atom("");
+
+const stepHistoryAtom = atom<string[]>([]);
 
 export const useStep = () => {
   const [step, setStep] = useAtom(stepAtom);
-  const [prevStep, setPrevStep] = useAtom(prevStepAtom);
-
-  const backStep = () => {
-    if (prevStep) {
-      console.log(prevStep);
-      setStep(prevStep);
-    } else {
-      console.warn("No previous step available to go back to.");
-    }
-  };
+  const [history, setHistory] = useAtom(stepHistoryAtom);
 
   const nextStep = (nextStep: string) => {
-    setPrevStep(step);
-    console.log(nextStep);
+    setHistory((prevHistory) => [...prevHistory, step]);
     setStep(nextStep);
+  };
+
+  const backStep = () => {
+    setHistory((prevHistory) => {
+      if (prevHistory.length === 0) {
+        console.warn("No previous step available to go back to.");
+        return prevHistory;
+      }
+
+      const newHistory = [...prevHistory];
+      const previousStep = newHistory.pop()!;
+
+      setStep(previousStep);
+
+      return newHistory;
+    });
   };
 
   const resetStep = () => {
     setStep("scan-nric");
-    setPrevStep("");
+    setHistory([]);
   };
 
   return {
     step,
-    backStep,
     nextStep,
+    backStep,
     resetStep,
+    history,
   };
 };
