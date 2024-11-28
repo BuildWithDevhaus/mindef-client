9ffd832e35b30dc17db60a4683a763fd19b54483
Agiff <ghifarimohammadrahadian@gmail.com>
2024-11-28 12:27:09 +0700

    fix: missing onSubmit

--- src/components/organisms/ActivityManualMeasurement.tsx
+++ src/components/organisms/ActivityManualMeasurement.tsx
@@ -53,6 +53,7 @@ const ActivityManualMeasurement: React.FC = () => {
           name="manual-measurement-form"
           id="manual-measurement-form"
           className="h-full w-full justify-between"
+          onSubmit={handleSubmit}
         >
           {step === "activity-manual-measurement-uniform-type" && (
             <SelectUniformType manualMeasurementInput={manualMeasurementInput} onConfirm={handleChange} nextStepDirection="activity-manual-measurement-shirt" />
