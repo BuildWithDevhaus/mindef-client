1ae6c054d47de66e5b47f522cab10490078b950a
Agiff <ghifarimohammadrahadian@gmail.com>
2024-12-16 15:23:05 +0700

    feat: populate divisions dropdown on user registration

--- src/features/UserRegistration/StepDivision.tsx
+++ src/features/UserRegistration/StepDivision.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useStep } from "../../hooks/useStep";
 import ButtonPrimary from "../../components/atoms/ButtonPrimary";
 import SelectOptionPrimary from "../../components/molecules/SelectOptionPrimary";
 import SelectOptionItem from "../../components/atoms/SelectOptionItem";
 import ButtonBack from "../../components/atoms/ButtonBack";
 import { UserRegistrationStepNextProps } from "../../types/staffSteps";
+import { DivisionSchema } from "../../zod/division";
+import { api } from "../../helpers/api";
 
 const StepDivision: React.FC<UserRegistrationStepNextProps> = ({
   userDetails,
@@ -12,8 +14,16 @@ const StepDivision: React.FC<UserRegistrationStepNextProps> = ({
   nextStepDestination,
   backOption
 }) => {
+  const [divisions, setDivisions] = useState<DivisionSchema[]>([]);
   const [inputValue, setInputValue] = useState(userDetails);
   const { nextStep } = useStep();
+
+  useEffect(() => {
+    (async () => {
+      const { data: divisionsData }: { data: DivisionSchema[] } = await api.get('/divisions');
+      setDivisions(divisionsData);
+    })();
+  }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setInputValue({ ...inputValue, divisionId: Number(e.target.value) });
@@ -29,9 +39,9 @@ const StepDivision: React.FC<UserRegistrationStepNextProps> = ({
       <div className="flex flex-col items-center justify-between h-full">
         <label className="text-6xl font-bold">Select your Unit/Wing</label>
         <SelectOptionPrimary placeholder="Select your Unit/Wing" value={inputValue.divisionId} onChange={handleChange} className="w-full">
-          <SelectOptionItem value="Alpha" text="Alpha" />
-          <SelectOptionItem value="Charlie" text="Charlie" />
-          <SelectOptionItem value="Gamma" text="Gamma" />
+          {divisions.map((division) => (
+            <SelectOptionItem key={division.id} value={String(division.id)} text={division.name} />
+          ))}
         </SelectOptionPrimary>
         <ButtonPrimary onClick={() => handleConfirm()} variant="large">Confirm</ButtonPrimary>
       </div>
