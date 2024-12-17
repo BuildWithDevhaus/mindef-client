a191af926e5bc5914cd6f71fc60f3d28b4659dd2
Agiff <ghifarimohammadrahadian@gmail.com>
2024-12-17 11:06:00 +0700

    feat: adding pantsInputSchema

--- src/zod/pants.ts
+++ src/zod/pants.ts
@@ -20,3 +20,17 @@ export const pantsSchema = z.object({
 });
 
 export type PantsSchema = z.infer<typeof pantsSchema>;
+
+export const pantsInputSchema = z.object({
+  id: z.number(),
+  rfidNo: z.string(),
+  belongsTo: z.string(),
+  uniformType: z.string(),
+  gender: z.string(),
+  waist: z.number(),
+  length: z.number(),
+  row: z.string(),
+  rack: z.string(),
+});
+
+export type PantsInputSchema = z.infer<typeof pantsInputSchema>;
