e002a9bb3d9f39a34527fde35c05217566d93508
Agiff <ghifarimohammadrahadian@gmail.com>
2024-12-17 10:39:33 +0700

    feat: adding shirtSchema and drawUniformSchema

---
+++ src/zod/shirt.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { drawUniformSchema } from "./drawUniform";
+
+export const shirtSchema = z.object({
+  id: z.number(),
+  rfidNo: z.string(),
+  belongsTo: z.string(),
+  uniformType: z.string(),
+  gender: z.string(),
+  shoulderLen: z.number(),
+  sleeve: z.number(),
+  collarLen: z.number(),
+  row: z.string(),
+  rack: z.string(),
+  status: z.string(),
+  deleteReasonId: z.number().optional(),
+  disposalDate: z.string().optional(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  drawUniform: z.array(drawUniformSchema),
+});
+
+export type ShirtSchema = z.infer<typeof shirtSchema>;
