6253d8613c9540c6e491815e7b871c8df51e4c32
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-12-19 10:17:22 +0800

    feat: function for add division

--- src/components/pages/AdminAddUnitWing.tsx
+++ src/components/pages/AdminAddUnitWing.tsx
@@ -4,18 +4,26 @@ import ButtonPrimary from "../atoms/ButtonPrimary";
 import InputFieldPrimary from "../atoms/InputFieldPrimary";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useDivision } from "../../hooks/useDivision";
+import { DivisionInputSchema } from "../../zod/division";
 
 const AdminAddUnitWing: React.FC = () => {
-  const [reason, setReason] = useState<string>("");
+  const [division, setDivision] = useState<DivisionInputSchema>({
+    name: "",
+  });
 
   const breadcrumbItems = [
     { label: "Admin Menu" },
     { label: "Unit/Wing", url: "/admin/unit-wing" },
+    { label: "Add Unit/Wing" },
   ];
 
+  const { createDivision } = useDivision();
+
   const handleButtonClick = () => {
-    if (reason.trim()) {
-      toast.success(`Unit/wing added: ${reason}`, {
+    if (division.name.trim()) {
+      createDivision(division);
+      toast.success(`Unit/wing added: ${division.name}`, {
         position: "bottom-right",
         autoClose: 3000,
         hideProgressBar: false,
@@ -24,6 +32,7 @@ const AdminAddUnitWing: React.FC = () => {
         draggable: true,
         progress: undefined,
       });
+      setDivision({ name: "" });
     } else {
       toast.error("Please enter a unit/wing", {
         position: "bottom-right",
@@ -43,13 +52,15 @@ const AdminAddUnitWing: React.FC = () => {
         <h2 className="text-2xl font-bold text-[#101828]">Add Unit/Wing</h2>
       </div>
       <div className="flex flex-col items-end gap-8">
-        <InputFieldPrimary
-          className="text-lg text-left"
-          placeholder="(Write new Unit/Wing)"
-          value={reason}
-          onChange={(e) => setReason(e.target.value)}
-        />
-        <ButtonPrimary onClick={handleButtonClick}>Confirm</ButtonPrimary>
+          <InputFieldPrimary
+            className="text-lg text-left"
+            placeholder="(Write new Unit/Wing)"
+            value={division.name}
+            onChange={(e) =>
+              setDivision((prev) => ({ ...prev, name: e.target.value }))
+            }
+          />
+          <ButtonPrimary onClick={handleButtonClick}>Confirm</ButtonPrimary>
       </div>
       <ToastContainer />
     </AdminLayout>
@@ -57,3 +68,4 @@ const AdminAddUnitWing: React.FC = () => {
 };
 
 export default AdminAddUnitWing;
+
