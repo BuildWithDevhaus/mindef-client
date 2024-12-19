1051aba5755dd5afac94687765406526f3e49fac
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-12-19 10:17:58 +0800

    feat: populate division table

--- src/components/pages/AdminUnitWing.tsx
+++ src/components/pages/AdminUnitWing.tsx
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Table from "../organisms/TableProps";
 import AdminLayout from "../templates/AdminLayout";
-import { unitWingData, unitWingHeaders } from "../../dummy/UnitWingDummy";
+import { unitWingHeaders } from "../../dummy/UnitWingDummy";
 import useTableFilter from "../../hooks/useTableFilter";
 import ButtonPrimary from "../atoms/ButtonPrimary";
-
-
+import TableAction from "../molecules/TableAction";
+import { capitalizeFirstLetter } from "../../helpers/wordStructure";
+import { getCurrentSlug } from "../../helpers/windows";
+import { useDivision } from "../../hooks/useDivision";
 
 const AdminUnitWing: React.FC = () => {
+  const {
+    searchQuery: divisionSearchQuery,
+    setSearchQuery: setDivisionSearchQuery,
+    rowsPerPage: divisionRowsPerPage,
+    setRowsPerPage: setDivisionRowsPerPage,
+    dateRange: divisionDateRange,
+    setDateRange: setDivisionDateRange,
+    filterDataByDateRange: filterDivisionDataByDateRange,
+  } = useTableFilter("", 5, { startDate: null, endDate: null }, "createdAt");
+
+  const { divisions, getDivisions, deleteDivision } = useDivision();
+  const [filteredDivisionData, setFilteredDivisionData] = useState<any[]>([]);
 
   const navigate = useNavigate();
 
-  const {
-    searchQuery: unitWingSearchQuery,
-    setSearchQuery: setUnitWingSearchQuery,
-    rowsPerPage: unitWingRowsPerPage,
-    setRowsPerPage: setUnitWingRowsPerPage,
-    dateRange: unitWingDateRange,
-    setDateRange: setUnitWingDateRange,
-    filterDataByDateRange: filterUnitWingDataByDateRange,
-  } = useTableFilter("", 5, { startDate: null, endDate: null }, [2,3]);
+  useEffect(() => {
+    getDivisions();
+  }, []);
 
-  const filteredUnitWingData = filterUnitWingDataByDateRange(unitWingData);
+  useEffect(() => {
+    if (divisions.length > 0) {
+      const filteredDivisions = filterDivisionDataByDateRange(divisions);
+
+      const mappedDivisions = filteredDivisions.map((division, index) => {
+        const handleEdit = () =>
+          navigate(`${getCurrentSlug()}/edit/${division.id}`);
+        const handleDelete = () => {
+          deleteDivision(division.id);
+        };
+
+        return [
+          index + 1,
+          capitalizeFirstLetter(division.name),
+          <TableAction
+            showEdit
+            showTrash
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />,
+        ];
+      });
+
+      setFilteredDivisionData(mappedDivisions);
+    }
+  }, [divisions, divisionDateRange]);
+
   const breadcrumbItems = [
-    { label: "Admin Menu"},
+    { label: "Admin Menu" },
     { label: "Unit/Wing", url: "/admin/unit-wing" },
   ];
 
@@ -32,21 +66,27 @@ const AdminUnitWing: React.FC = () => {
     <AdminLayout headingText="Unit/Wing" breadcrumbItems={breadcrumbItems}>
       <div className="mb-8 flex items-center justify-between">
         <h2 className="text-2xl font-bold text-[#101828]">List of Unit/Wing</h2>
-        <ButtonPrimary onClick={() => {navigate('/admin/unit-wing/add')}}>Add Unit/Wing</ButtonPrimary>
+        <ButtonPrimary
+          onClick={() => {
+            navigate("/admin/unit-wing/add");
+          }}
+        >
+          Add Unit/Wing
+        </ButtonPrimary>
       </div>
       <Table
         headers={unitWingHeaders}
-        data={filteredUnitWingData}
-        rowsPerPage={unitWingRowsPerPage}
-        enablePagination={true}
-        enableSearch={true}
-        enableRowsPerPage={true}
+        data={filteredDivisionData}
+        rowsPerPage={divisionRowsPerPage}
+        enablePagination
+        enableSearch
+        enableRowsPerPage
         enableDateRange={false}
-        initialSearchQuery={unitWingSearchQuery}
-        onSearchChange={setUnitWingSearchQuery}
-        onRowsPerPageChange={setUnitWingRowsPerPage}
-        onDateRangeChange={setUnitWingDateRange}
-        dateRange={unitWingDateRange}
+        initialSearchQuery={divisionSearchQuery}
+        onSearchChange={setDivisionSearchQuery}
+        onRowsPerPageChange={setDivisionRowsPerPage}
+        onDateRangeChange={setDivisionDateRange}
+        dateRange={divisionDateRange}
       />
     </AdminLayout>
   );
