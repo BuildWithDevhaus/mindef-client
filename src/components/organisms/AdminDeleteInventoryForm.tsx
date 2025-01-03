65f98b74e8cbeb9336135dd9acb8cb51d53376ce
KyleFinnTuan <wandinainggolan9@gmail.com>
2025-01-03 20:45:14 +0800

    chores: remove unused import

--- src/components/organisms/AdminDeleteInventoryForm.tsx
+++ src/components/organisms/AdminDeleteInventoryForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useStep } from '../../hooks/useStep';
 import AdminScanRfid from './AdminScanRfid';
 import AdminDeleteInventoryReason from '../../features/AdminDeleteInventory/AdminDeleteInventoryReason';
