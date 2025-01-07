2b0b2f1b217c1bb18b4887b21c7a575c1b0f8b49
KyleFinnTuan <wandinainggolan9@gmail.com>
2025-01-07 13:27:35 +0800

    refactor: change alert to toaster

--- src/components/pages/AdminEditPin.tsx
+++ src/components/pages/AdminEditPin.tsx
@@ -1,68 +1,79 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ButtonPrimary from "../atoms/ButtonPrimary";
+import ContainerLayout from "../templates/ContainerLayout";
+import { ToastContainer } from "react-toastify";
+import { toastAlert } from "../../helpers/toastAlert";
 
 const EditPin: React.FC = () => {
-  const [newPin, setNewPin] = useState('');
-  const [confirmPin, setConfirmPin] = useState('');
+  const [newPin, setNewPin] = useState("");
+  const [confirmPin, setConfirmPin] = useState("");
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
 
   const handleEditPin = () => {
     if (newPin.length !== 7 || confirmPin.length !== 7) {
-      setError('PIN must be exactly 7 digits.');
+      toastAlert("error", "PIN must be exactly 7 digits.");
       return;
     }
 
     if (newPin !== confirmPin) {
-      setError('Pins do not match!');
+      toastAlert("error", "Pins do not match!");
       return;
     }
-    localStorage.setItem('pin', newPin);
-    localStorage.removeItem('isAuthenticated');7
+    localStorage.setItem("pin", newPin);
+    localStorage.removeItem("isAuthenticated");
+    7;
     setError(null);
-    navigate('/admin/login', {
-      state: { toastMessage: 'PIN updated successfully! You need to login again.' },
+    navigate("/admin/login", {
+      state: {
+        toastMessage: "PIN updated successfully! You need to login again.",
+      },
     });
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <h1 className="text-5xl font-bold mb-6">Edit PIN</h1>
+      <ContainerLayout>
+        <div className="p-3 gap-3 flex flex-col items-center">
+          <h1 className="text-5xl font-bold mb-6">Edit PIN</h1>
 
-      {/* Error Message */}
-      {error && <p className="text-red-500 mb-4">{error}</p>}
+          {/* Error Message */}
+          {error && <p className="text-red-500 mb-4">{error}</p>}
+          <div>
+            <div className="mb-4">
+              <input
+                type="password"
+                placeholder="Enter new PIN (7 digits)"
+                className="border rounded-lg p-2 w-80"
+                value={newPin}
+                onChange={(e) => setNewPin(e.target.value.replace(/\D/g, ""))} // Ensure only digits
+                maxLength={7}
+              />
+            </div>
 
-      {/* New PIN Input */}
-      <div className="mb-4">
-        <input
-          type="password"
-          placeholder="Enter new PIN (7 digits)"
-          className="border rounded-lg p-2 w-80"
-          value={newPin}
-          onChange={(e) => setNewPin(e.target.value.replace(/\D/g, ''))} // Ensure only digits
-          maxLength={7}
-        />
-      </div>
-
-      {/* Confirm PIN Input */}
-      <div className="mb-4">
-        <input
-          type="password"
-          placeholder="Confirm new PIN (7 digits)"
-          className="border rounded-lg p-2 w-80"
-          value={confirmPin}
-          onChange={(e) => setConfirmPin(e.target.value.replace(/\D/g, ''))} // Ensure only digits
-          maxLength={7}
-        />
-      </div>
-
-      {/* Edit Button */}
-      <button
-        className="bg-[#2F6D57] text-white p-3 rounded-lg w-80"
-        onClick={handleEditPin}
-      >
-        Save Changes
-      </button>
+            {/* Confirm PIN Input */}
+            <div className="mb-4">
+              <input
+                type="password"
+                placeholder="Confirm new PIN (7 digits)"
+                className="border rounded-lg p-2 w-80"
+                value={confirmPin}
+                onChange={(e) =>
+                  setConfirmPin(e.target.value.replace(/\D/g, ""))
+                } // Ensure only digits
+                maxLength={7}
+              />
+            </div>
+          </div>
+          {/* New PIN Input */}
+          {/* Edit Button */}
+          <ButtonPrimary className="w-80" onClick={handleEditPin}>
+            Save Changes
+          </ButtonPrimary>
+        </div>
+      </ContainerLayout>
+      <ToastContainer />
     </div>
   );
 };
