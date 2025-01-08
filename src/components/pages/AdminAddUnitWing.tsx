8cceef4890f777a4c5794a016c50bf0a8fb9cc19
KyleFinnTuan <wandinainggolan9@gmail.com>
2025-01-08 10:40:53 +0800

    feat: make enter keypress can trigger confirm

--- src/components/pages/AdminAddUnitWing.tsx
+++ src/components/pages/AdminAddUnitWing.tsx
@@ -8,6 +8,7 @@ import { useUnitWing } from "../../hooks/useUnitWing";
 import { UnitWingInputSchema } from "../../zod/unitWing";
 import { toastAlert } from "../../helpers/toastAlert";
 import { useNavigate } from "react-router-dom";
+import useKeyPress from "../../helpers/useKeyPress";
 
 const AdminAddUnitWing: React.FC = () => {
   const navigate = useNavigate();
@@ -49,22 +50,24 @@ const AdminAddUnitWing: React.FC = () => {
     }
   };
 
+  useKeyPress("Enter", handleButtonClick);
+
   return (
     <AdminLayout headingText="Unit/Wing" breadcrumbItems={breadcrumbItems}>
       <div className="mb-8 flex items-center justify-between">
         <h2 className="text-2xl font-bold text-[#101828]">Add Unit/Wing</h2>
       </div>
       <div className="flex flex-col items-end gap-8">
-          <InputFieldPrimary
-            className="text-lg text-left"
-            placeholder="(Write new Unit/Wing)"
-            value={unitWing.name}
-            onChange={(e) =>
-              setUnitWing((prev) => ({ ...prev, name: e.target.value }))
-            }
+        <InputFieldPrimary
+          className="text-lg text-left"
+          placeholder="(Write new Unit/Wing)"
+          value={unitWing.name}
+          onChange={(e) =>
+            setUnitWing((prev) => ({ ...prev, name: e.target.value }))
+          }
           ref={inputRef}
-          />
-          <ButtonPrimary onClick={handleButtonClick}>Confirm</ButtonPrimary>
+        />
+        <ButtonPrimary onClick={handleButtonClick}>Confirm</ButtonPrimary>
       </div>
       <ToastContainer />
     </AdminLayout>
